c562267b5defa4549337df7bb8f97425ecce437c
z1p <z1pti3@outlook.com>
2021-09-04 15:43:20 +0000

    Added form loading

--- web/webTrigger.py
+++ web/webTrigger.py
@@ -14,5 +14,28 @@ def custom_static(file):
 
 @pluginPages.route("/",methods=["GET"])
 def mainPage():
-    triggers = trigger._webTrigger().query(sessionData=jimi.api.g.sessionData,query={},fields=["_id","name","icon","formData"])["results"]
+    triggers = trigger._webTrigger().query(sessionData=jimi.api.g.sessionData,query={},fields=["_id","name","icon"])["results"]
     return render_template("webTrigger.html", triggers=triggers)
+
+@pluginPages.route("/<webTriggerID>/",methods=["GET"])
+def getForm(webTriggerID):
+    try:
+        webTrigger = trigger._webTrigger().query(sessionData=jimi.api.g.sessionData,id=webTriggerID,fields=["_id","name","formData"])["results"][0]
+        # Process and correct formData
+        for field in webTrigger["formData"]:
+            field["schemaitem"] = field["schema_item"]
+            if field["type"] == "input":
+                field["textbox"] = ""
+            elif field["type"] == "json-input":
+                field["textbox"] = {}
+            elif field["type"] == "checkbox":
+                field["checked"] = False
+            elif field["type"] == "group-checkbox":
+                field["checked"] = False
+            elif field["type"] == "dropdown":
+                field["current"] = ""
+            field["tooltip"] = field["description"]
+        return { "result" : webTrigger }
+    except:
+        return {}, 403
+
