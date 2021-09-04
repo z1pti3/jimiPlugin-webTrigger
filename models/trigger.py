8425a24192d2e2109c586054cac030fd9c347bfc
z1p <z1pti3@outlook.com>
2021-09-04 11:36:23 +0000

    Updated UI and classes

--- models/trigger.py
+++ models/trigger.py
@@ -1,5 +1,22 @@
+import time
+
 import jimi
+
+from plugins.webTrigger.models import webTrigger
 
 class _webTrigger(jimi.trigger._trigger):
     icon = str()
-    fromData = list()
+    formData = list()
+    limit = 10
+
+    def check(self):
+        events = []
+        bulkClass = jimi.db._bulk()
+        webTriggerEvents = webTrigger._webTriggerEvent().getAsClass(query={ "webTriggerID" : self._id, "runTime" : 0 },limit=self.limit)["results"]
+        for event in webTriggerEvents:
+            event.runtime = time.time()
+            event.bulkUpdate(["runTime"],bulkClass)
+            events.append(event.formResult)
+            events[-1]["webTriggerExecutionID"] = event.webTriggerExecutionID
+        bulkClass.bulkOperatonProcessing()
+        self.result["events"] = events
--- models/webTrigger.py
+++ models/webTrigger.py
@@ -1,15 +1,32 @@
+import uuid
+
 import jimi
+
+class _webTriggerEvent(jimi.db._document):
+    webTriggerID = str()
+    formResult = dict()
+    runTime = int()
+    webTriggerExecutionID = str()
+
+    _dbCollection = jimi.db.db["webTriggerEvent"]
+
+    def new(self, acl, webTriggerID, fromData):
+        self.acl = acl
+        self.webTriggerID = webTriggerID
+        self.fromData = fromData
+        self.webTriggerExecutionID = str(uuid.uuid4())
+        return super(_webTriggerEvent, self).new()
 
 class _webTriggerOutput(jimi.db._document):
     webTriggerID = str()
     webTriggerExecutionID = str()
-    output_data = str()
+    outputData = str()
 
     _dbCollection = jimi.db.db["webTriggerOutput"]
 
-    def new(self, acl, webTriggerID, webTriggerExecutionID, output_data):
+    def new(self, acl, webTriggerID, webTriggerExecutionID, outputData):
         self.acl = acl
         self.webTriggerID = webTriggerID
         self.webTriggerExecutionID = webTriggerExecutionID
-        self.output_data = output_data
+        self.outputData = outputData
         return super(_webTriggerOutput, self).new()
