ca20a324c31fd82b352bbe916e88a1f00f2f35a2
z1pti3 <z1pti3@outlook.com>
2021-11-20 12:34:03 +0000

    Added try excpet so webTriggeroutput does not crash when ran on flows without webTrigger

--- models/action.py
+++ models/action.py
@@ -9,16 +9,22 @@ class _webTriggerAddOutput(jimi.action._action):
     outputType = "content"
 
     def doAction(self,data):
-        if self.webTriggerID:
-            webTriggerID = jimi.helpers.evalString(self.webTriggerID,{"data" : data["flowData"], "eventData" : data["eventData"], "conductData" : data["conductData"], "persistentData" :  data["persistentData"] })
-        else:
-            webTriggerID = data["flowData"]["event"]["webTriggerID"]
-        if self.webTriggerExecutionID:
-            webTriggerExecutionID = jimi.helpers.evalString(self.webTriggerExecutionID,{"data" : data["flowData"], "eventData" : data["eventData"], "conductData" : data["conductData"], "persistentData" :  data["persistentData"] })
-        else:
-            webTriggerExecutionID = data["flowData"]["event"]["webTriggerExecutionID"]
+        try:
+            if self.webTriggerID:
+                webTriggerID = jimi.helpers.evalString(self.webTriggerID,{"data" : data["flowData"], "eventData" : data["eventData"], "conductData" : data["conductData"], "persistentData" :  data["persistentData"] })
+            else:
+                webTriggerID = data["flowData"]["event"]["webTriggerID"]
+            if self.webTriggerExecutionID:
+                webTriggerExecutionID = jimi.helpers.evalString(self.webTriggerExecutionID,{"data" : data["flowData"], "eventData" : data["eventData"], "conductData" : data["conductData"], "persistentData" :  data["persistentData"] })
+            else:
+                webTriggerExecutionID = data["flowData"]["event"]["webTriggerExecutionID"]
+        except:
+            return { "result" : True, "rc" : 1, "msg" : "webTriggerExecutionID not found"  }
 
-        output_data = jimi.helpers.evalString(self.output_data,{"data" : data["flowData"], "eventData" : data["eventData"], "conductData" : data["conductData"], "persistentData" :  data["persistentData"] })
-        outputDict = {"type":self.outputType,"content":output_data}
-        webTrigger._webTriggerOutput().new(self.acl,webTriggerID,webTriggerExecutionID,outputDict)
-        return { "result" : True, "rc" : 0  }
+        try:
+            output_data = jimi.helpers.evalString(self.output_data,{"data" : data["flowData"], "eventData" : data["eventData"], "conductData" : data["conductData"], "persistentData" :  data["persistentData"] })
+            outputDict = {"type":self.outputType,"content":output_data}
+            webTrigger._webTriggerOutput().new(self.acl,webTriggerID,webTriggerExecutionID,outputDict)
+            return { "result" : True, "rc" : 0  }
+        except:
+            return { "result" : True, "rc" : 2, "msg" : "Error adding webTrigger output"  }
